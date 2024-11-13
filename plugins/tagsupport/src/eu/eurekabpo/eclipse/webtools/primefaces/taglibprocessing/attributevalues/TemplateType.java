5d73d4682d2cc3dc96c7d5ab7678408aa07e5b06
eureka-bpo <support@eureka-bpo.eu>
2024-11-13 20:56:17 +0100

    feat: implement constructor for paginator templates

---
+++ plugins/tagsupport/src/eu/eurekabpo/eclipse/webtools/primefaces/taglibprocessing/attributevalues/TemplateType.java
@@ -0,0 +1,32 @@
+package eu.eurekabpo.eclipse.webtools.primefaces.taglibprocessing.attributevalues;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.eclipse.jst.jsf.context.resolver.structureddocument.IDOMContextResolver;
+import org.eclipse.jst.jsf.context.resolver.structureddocument.IStructuredDocumentContextResolverFactory;
+import org.eclipse.jst.jsf.metadataprocessors.features.PossibleValue;
+import org.eclipse.jst.jsf.taglibprocessing.attributevalues.StringType;
+
+public class TemplateType extends StringType {
+
+	@Override
+	public List getPossibleValues() {
+		List<PossibleValue> possibleValues = super.getPossibleValues();
+		String currentValue = getCurrentAttrValue();
+		return possibleValues.stream()
+			.peek(val -> val.setValue(currentValue + val.getValue()))
+			.collect(Collectors.toList());
+	}
+	
+	private String getCurrentAttrValue() {
+		if (getStructuredDocumentContext() != null){
+			IDOMContextResolver resolver = IStructuredDocumentContextResolverFactory.INSTANCE.getDOMContextResolver(getStructuredDocumentContext());
+			if (resolver != null){
+				return resolver.getNode().getNodeValue();
+			}
+		}
+		return ""; //$NON-NLS-1$
+
+	}
+}
